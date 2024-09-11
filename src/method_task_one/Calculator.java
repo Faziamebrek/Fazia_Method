7c19e784e2ec397f633bb3bee7aea7d97c89c31f
faziamebrek <faziamebrek@gmail.com>
2024-09-11 11:52:54 -0700

    Calculator

---
+++ src/method_task_one/Calculator.java
@@ -0,0 +1,31 @@
+package method_task_one;
+
+public class Calculator {
+    public void getCalculation(int num1, int num2, char ch) {
+
+        int a = 18;
+        int b = 6;
+        switch (ch) {
+            case ('+'):
+                System.out.println(a + b);
+                break;
+            case ('-'):
+                System.out.println(a - b);
+                break;
+            case ('*'):
+                System.out.println(a * b);
+                break;
+            case ('/'):
+                System.out.println(a / b);
+                break;
+            case ('%'):
+                System.out.println(a % b);
+                break;
+            default:
+                System.out.println(" invalid operator");
+
+        }
+
+
+    }
+}
