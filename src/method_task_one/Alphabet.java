ab67d3985cd20cd9e56ddb84477ea8d2a96071b4
faziamebrek <faziamebrek@gmail.com>
2024-09-11 11:46:17 -0700

    Alphabet

---
+++ src/method_task_one/Alphabet.java
@@ -0,0 +1,21 @@
+package method_task_one;
+
+public class Alphabet {
+    public boolean isAlphabet(char c) {
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+            return true;
+        } else {
+            return false;
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Alphabet alphabet = new Alphabet();
+        System.out.println(alphabet.isAlphabet('p'));
+
+    }
+
+
+}
+
