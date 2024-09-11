f06dc79a8c2a5e789da25249a27c3e9d82c6dfe3
faziamebrek <faziamebrek@gmail.com>
2024-09-11 13:11:54 -0700

    DivisibleBy5

---
+++ src/method_task_one/DivisibleByFive.java
@@ -0,0 +1,20 @@
+package method_task_one;
+
+public class DivisibleByFive {
+    //write java Program to Check Whether Number is Divisible by 5
+    public boolean isDivisible(int num) {
+        if (num % 5 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        DivisibleByFive divisibleByFive = new DivisibleByFive();
+        divisibleByFive.isDivisible(15);
+        System.out.println(divisibleByFive.isDivisible(21));
+    }
+
+}
