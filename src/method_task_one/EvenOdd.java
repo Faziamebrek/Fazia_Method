0e9e6176e99026bbe174cbb4f5b9fec7d0e71511
faziamebrek <faziamebrek@gmail.com>
2024-09-11 20:51:06 -0700

    OddEven

--- src/method_task_one/EvenOdd.java
+++ src/method_task_one/EvenOdd.java
@@ -1,4 +1,30 @@
 package method_task_one;
+public class EvenOdd {
+    public void PrintEvenOdd (int a){
+
+        if (a % 2 == 0) {
+            System.out.println(a+ "is an even Number ");
+
+        } else {
+            System.out.println( a+ "is an Odd Number");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        EvenOdd evenOdd = new EvenOdd();
+        evenOdd.PrintEvenOdd(15);
+
+    }
+}
+
+
+
+
+
+
+
+
 
 
 
